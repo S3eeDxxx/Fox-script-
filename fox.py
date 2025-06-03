17f1b5060f6c11d4d44fed29416c95470b8e7e5e
S3eeDxxx <omarmitro144@gmail.com>
2025-06-03 13:00:41 +0300

    fox.py
    import os
    
    print("✅ بدأ تشغيل سكريبت ngrok الخاص بفوكس...")
    
    # 1️⃣ أمر التثبيت الكامل
    os.system('curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
      | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
      && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
      | sudo tee /etc/apt/sources.list.d/ngrok.list \
      && sudo apt update \
      && sudo apt install ngrok')
    
    # 2️⃣ أمر إضافة التوكن
    os.system('ngrok config add-authtoken 2xuAIS8zlgXDXfYLIOSo5VUXrwf_7CZ6LVvxrpRY2fJ68RW5q')
    
    # 3️⃣ أمر تشغيل النفق
    os.system('ngrok http --url=luckily-meet-hen.ngrok-free.app 8080')

---
+++ fox.py
@@ -0,0 +1,17 @@
+import os
+
+print("✅ بدأ تشغيل سكريبت ngrok الخاص بفوكس...")
+
+# 1️⃣ أمر التثبيت الكامل
+os.system('curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+  | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+  && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+  | sudo tee /etc/apt/sources.list.d/ngrok.list \
+  && sudo apt update \
+  && sudo apt install ngrok')
+
+# 2️⃣ أمر إضافة التوكن
+os.system('ngrok config add-authtoken 2xuAIS8zlgXDXfYLIOSo5VUXrwf_7CZ6LVvxrpRY2fJ68RW5q')
+
+# 3️⃣ أمر تشغيل النفق
+os.system('ngrok http --url=luckily-meet-hen.ngrok-free.app 8080')
